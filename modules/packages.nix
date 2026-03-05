5c1a22d185e68d2e36c34ab770ea45d5bfb1b09e
emgardner <gardner.ethan10@gmail.com>
2026-03-05 09:07:19 -0500

    sync

--- modules/packages.nix
+++ modules/packages.nix
@@ -38,5 +38,6 @@
     lld
     element-desktop
     zulip
+    typos
   ];
 }
--- modules/zsh.nix
+++ modules/zsh.nix
@@ -1,14 +1,33 @@
-{lib, ...}: {
+{
+  lib,
+  pkgs,
+  ...
+}: {
   programs.zsh = {
     enable = true;
     enableCompletion = true;
     autosuggestion.enable = true;
     syntaxHighlighting.enable = true;
     history.size = 10000;
+    plugins = [
+      {
+        name = "powerlevel10k-config";
+        src = ./p10k;
+        file = "p10k.zsh";
+      }
+      {
+        name = "zsh-powerlevel10k";
+        src = "${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/";
+        file = "powerlevel10k.zsh-theme";
+      }
+    ];
 
     oh-my-zsh = {
       enable = true;
-      plugins = ["git" "thefuck"];
+      plugins = [
+        "git"
+        "thefuck"
+      ];
       theme = "robbyrussell";
     };
 
